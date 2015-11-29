b4b699c398659b711eeec02ae6655dde690a2d33
Maks Vyhryst <vyhrystmax@gmail.com>
2015-11-29 23:23:57 +0200

    proxy form begin

--- src/Epar/ServiceBundle/Controller/DefaultController.php
+++ src/Epar/ServiceBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Epar\ServiceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -12,9 +14,28 @@ class DefaultController extends Controller
         $securityContext = $this->container->get('security.authorization_checker');
 
         if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->render('EparServiceBundle:Default:index.html.twig');
+
+            $form = $this->createFormBuilder()
+                ->add('proxy', 'text')
+                ->add('port', 'text')
+                ->add('save', 'submit', array('label' => 'Submit'))
+                ->setAction($this->generateUrl('epar_ajax_ebay_proxy_sett'))
+                ->setAttribute('onsubmit','54555')
+                ->getForm();
+
+            return $this->render('EparServiceBundle:Default:index.html.twig', array(
+                'proxy_form'    => $form->createView()
+            ));
+
         } else {
             return $this->render('EparServiceBundle:Default:index_login.html.twig');
         }
+    }
+
+    public function proxySettingsAction(Request $oRequest)
+    {
+        fwrite(fopen('/tmp/dump', 'a'), print_r(312, 1) . "\n");
+
+        return new Response('hYI PIZDA');
     }
 }
--- src/Epar/ServiceBundle/Resources/public/js/Epar.js
+++ src/Epar/ServiceBundle/Resources/public/js/Epar.js
@@ -1,4 +1,14 @@
 /**
  * Created by rg on 28.11.15.
  */
-var Epar = {};
+var Epar = {
+    ajax: {
+        proxySettings: function() {
+            var form = jQuery(form);
+
+            $.post(form.attr('action') , form.serialize() );
+
+            return false;
+        }
+    }
+};
